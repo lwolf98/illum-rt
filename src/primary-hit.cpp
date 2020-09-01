b62992741b3fc7c9474980459bef200c75cb1ca2
Kai Selgrad <kai.selgrad@oth-regensburg.de>
2020-09-01 09:46:25 +0200

    apply emissive to direct hit

--- src/primary-hit.cpp
+++ src/primary-hit.cpp
@@ -11,23 +11,26 @@ using namespace std;
 gi_algorithm::sample_result primary_hit_display::sample_pixel(uint32_t x, uint32_t y, uint32_t samples, const render_context &rc) {
 	sample_result result;
 	for (int sample = 0; sample < samples; ++sample) {
+		glm::vec3 radiance(0);
 		ray ray = cam_ray(rc.scene.camera, x, y, glm::vec2(rc.rng.uniform_float()-0.5f, rc.rng.uniform_float()-0.5f));
 		triangle_intersection closest = rc.scene.rt->closest_hit(ray);
 		if (closest.valid()) {
 			diff_geom dg(closest, rc.scene);
-			//auto col = dg.mat->albedo_tex ? dg.mat->albedo_tex->sample(dg.tc) : dg.mat->albedo;
-			lambertian_reflection brdf;
-			auto [l_id, l_pdf] = rc.scene.light_distribution->sample_index(rc.rng.uniform_float());
-			light *l = rc.scene.lights[l_id];
-			auto [shadow_ray,col,pdf] = l->sample_Li(dg, rc.rng.uniform_float2());
-			vec3 sample(0);
-			if (auto is = rc.scene.rt->closest_hit(shadow_ray); !is.valid() || is.t >= shadow_ray.max_t) {
-				sample = col * brdf.f(dg, -ray.d, shadow_ray.d) * cdot(shadow_ray.d, dg.ns) / (pdf * l_pdf);
+			if (dg.mat->emissive != vec3(0)) {
+				radiance = dg.mat->emissive;
 			}
-			result.push_back({sample,vec2(0)});
+			else {
+				//auto col = dg.mat->albedo_tex ? dg.mat->albedo_tex->sample(dg.tc) : dg.mat->albedo;
+				lambertian_reflection brdf;
+				auto [l_id, l_pdf] = rc.scene.light_distribution->sample_index(rc.rng.uniform_float());
+				light *l = rc.scene.lights[l_id];
+				auto [shadow_ray,col,pdf] = l->sample_Li(dg, rc.rng.uniform_float2());
+				if (auto is = rc.scene.rt->closest_hit(shadow_ray); !is.valid() || is.t >= shadow_ray.max_t) {
+					radiance = col * brdf.f(dg, -ray.d, shadow_ray.d) * cdot(shadow_ray.d, dg.ns) / (pdf * l_pdf);
+				}
+			}
 		}
-		else
-			result.push_back({vec3(0,0,0),vec2(0)});
+		result.push_back({radiance,vec2(0)});
 	}
 	return result;
 }
