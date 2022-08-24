8a63c4ea28e5b4bfe58e4792cd337458bcb43312
Kai Selgrad <kai.selgrad@oth-regensburg.de>
2022-08-24 21:50:03 +0200

    remove duplicate template parameters in step-generator

--- libgi/step-gen.lisp
+++ libgi/step-gen.lisp
@@ -5,10 +5,11 @@
 	    (public
 	      (decl ((static constexpr char id[] = ,id)))
 	      ,(cl:if data
-		 `(function use ,(loop for (nil name basetype) in data collect `(',basetype (dref ',name))) pure -> void)))))
+		 `(function use ,(loop for (nil name basetype) in data collect `(,basetype (dref ',name))) pure -> void)))))
      ,(cl:if data
 	 `(namespace wire
-	    (template ,(loop for (type nil nil) in data collect `(typename ',type))
+	    (template ,(remove-duplicates (loop for (type nil nil) in data collect `(typename ',type))
+					  :from-end t :test #'equal)
 	       (class ',name ((public (from-namespace wf ',name)))
 		 (public
 		   (using (from-namespace wf ',name ',name))
@@ -16,7 +17,7 @@
 		                collect `(',type (dref ',name) = nullptr))  ;; dref is a stupid hack
 		     (function properly-wired () -> bool
 		       (return (&& ,@(loop for (nil name nil) in data collect name))))
-		     (function use ,(loop for (nil name basetype) in data collect `(',basetype (dref ',name))) -> void
+		     (function use ,(loop for (nil name basetype) in data collect `(,basetype (dref ',name))) -> void
 		       ,@(loop for (type name nil) in data
 			       collect `(set (pref this ',name) (dynamic-cast ',type (dref nil) ',name)))) ;; dref nil! :/
 		     ,(cl:if run `(function run () override -> void
@@ -24,4 +25,5 @@
 				    (funcall (from-namespace wf ',name run))))))))))))
 
 
-
+(defmacro per-sample (type)
+  `(instantiate per_sample_data (,type)))
