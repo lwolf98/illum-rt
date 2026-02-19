585f3459dd78231d073e018b2b2f069f2137aad0
Lukas Wolf <lukas.wolf@st.oth-regensburg.de>
2026-02-20 00:14:42 +0100

    Update Evaluation

---
+++ load_models/cache_frog.sh
@@ -0,0 +1,17 @@
+#./rtgi_app0_comp0_ORIGINAL -l scripts/cache/cache_frog -p
+./rtgi_app0_comp0_ORIGINAL -s scripts/cache/cache_frog
+echo "Finished 1"
+
+#./rtgi_app2_comp9_SH_Q_Z -s scripts/cache/cache_frog
+#echo "Finished 1"
+./rtgi_app2_comp1_Q -s scripts/cache/cache_frog
+echo "Finished 2"
+./rtgi_app2_comp2_SC -s scripts/cache/cache_frog
+echo "Finished 3"
+./rtgi_app2_comp2_SH -s scripts/cache/cache_frog
+echo "Finished 4"
+./rtgi_app2_comp9_SH_Q -s scripts/cache/cache_frog
+echo "Finished 5"
+
+#./rtgi_app1_comp9_SH_Q_Z -s scripts/cache/cache_frog
+#echo "Finished 6"
