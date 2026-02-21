546e402b15af7e1bae75d914e8ba386318bfa7a0
Lukas Wolf <lukas.wolf@st.oth-regensburg.de>
2026-02-21 10:35:05 +0100

    Update Evaluation

--- load_models/cache_frog.sh
+++ load_models/cache_frog.sh
@@ -4,14 +4,23 @@ echo "Finished 1"
 
 #./rtgi_app2_comp9_SH_Q_Z -s scripts/cache/cache_frog
 #echo "Finished 1"
-./rtgi_app2_comp1_Q -s scripts/cache/cache_frog
-echo "Finished 2"
-./rtgi_app2_comp2_SC -s scripts/cache/cache_frog
-echo "Finished 3"
-./rtgi_app2_comp2_SH -s scripts/cache/cache_frog
-echo "Finished 4"
-./rtgi_app2_comp9_SH_Q -s scripts/cache/cache_frog
+#./rtgi_app2_comp1_Q -s scripts/cache/cache_frog
+#echo "Finished 2"
+#./rtgi_app2_comp2_SC -s scripts/cache/cache_frog
+#echo "Finished 3"
+#./rtgi_app2_comp6_SH -s scripts/cache/cache_frog
+#echo "Finished 4"
+./rtgi_app2_comp8_SH_Q -s scripts/cache/cache_frog
 echo "Finished 5"
 
+#---
 #./rtgi_app1_comp9_SH_Q_Z -s scripts/cache/cache_frog
 #echo "Finished 6"
+#./rtgi_app1_comp1_Q -s scripts/cache/cache_frog
+#echo "Finished 7"
+#./rtgi_app1_comp2_SC -s scripts/cache/cache_frog
+#echo "Finished 8"
+#./rtgi_app1_comp6_SH -s scripts/cache/cache_frog
+#echo "Finished 9"
+./rtgi_app1_comp8_SH_Q -s scripts/cache/cache_frog
+echo "Finished 10"
